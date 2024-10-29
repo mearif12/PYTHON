80859de0c42c943f835a0b833023630fa64ccbf8
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-10-29 04:14:13 -0700

    output & data types

---
+++ main1.py
@@ -0,0 +1,7 @@
+#1.Printing and concatenating different data-types 
+
+name = "Arif " ; roll = 12 ; bool = True
+  
+  #for same type use "+",use "," for differnet
+
+print("Hello " + name , roll , bool)#Hello Arif 12 True 
