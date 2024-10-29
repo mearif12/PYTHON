a07cdb0b3302ebc630ffa0fc866f7e8a4d8ac567
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-10-29 04:16:47 -0700

    operators

---
+++ main2.py
@@ -0,0 +1,14 @@
+#2.Updating and Performing simple (+,-,*,/)
+
+ #The execution of code happens line by line.so after getting 1st error it terminates execution.
+
+ #python is a case-sensitive language.use snake casing to declare variables:(my_name)
+
+x = 5 ; 
+x = 7 ; #update to 7 
+y = 3 
+
+print("Addition: ", x+y) #10
+print("Subtraction: ", x-y) #4
+print("Multiplication: ", x*y) #21
+print("Division: ",x/y) #2.3333
