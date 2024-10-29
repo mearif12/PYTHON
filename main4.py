1f668a08a26ee9240be9c7c1930cb6a83a5604bd
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-10-29 04:20:03 -0700

    comparison & logical operator

---
+++ main4.py
@@ -0,0 +1,11 @@
+#4.Comparison Operator (>,<,>=,<=) and Logical Operator (and,or)
+
+   #comparison & logical operator has only Boolean output: True/False.
+   #Iteration is about executing an instruction repeatedly. 
+   #Iteration is commonly represented as a loop.
+
+a = 5 ; b = 7
+
+print(60 > 30)#True
+print (True and False)#False
+print(a > 10 or b > 3)#True
